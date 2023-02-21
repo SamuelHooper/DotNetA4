76db7444a8e987745c277207efb84d7de982f565
Samuel Hooper <shooper3@my.wctc.edu>
2023-02-20 19:39:54 -0600

    Small Code refactors

--- Program.cs
+++ Program.cs
@@ -19,83 +19,108 @@ internal class Program
 
             if (menuOption == "1" && File.Exists(file))
             {
-                // nuget Trival.Console (7.0.0) package for paging
-                // It does have some bugs with press the down arrow keys too fast
-                List<Movie> movies = GetMoviesList(file);
-                var col = new Trivial.Collection.SelectionData<string>();
-                for (int i = 0; i < movies.Count; i++)
-                {
-                    // Add movie to display
-                    col.Add(movies[i].DisplayMovie());
-                }
-
-                // Create options for display
-                var options = new SelectionConsoleOptions
-                {
-                    // We can define the colors of the item selected.
-                    SelectedForegroundConsoleColor = ConsoleColor.Black,
-
-                    // Not select item color
-                    ItemForegroundConsoleColor = ConsoleColor.White,
-
-                    Tips = "Tips: You can use arrow key to select and press ENTER key to exit.",
-                    TipsForegroundConsoleColor = ConsoleColor.Yellow,
-                    SelectedPrefix = "> ",
-                    Prefix = " ",
-                    Column = 2,
-                    MaxRow = 20
-                };
-
-                // Pause to let user read and and see full description of selected item
-                DefaultConsole.Select(col, options);
-                Console.WriteLine(); // Spacer line for returning to main menu
-
+                DisplayAllMovies(file);
             }
             else if (menuOption == "2" && File.Exists(file))
             {
-                // Loop to add movies
-                do
-                {
-                    List<Movie> movies = GetMoviesList(file);
-                    List<string> lowerCaseMovieTitles = new();
-                    Movie newMovie = new();
-                    Console.Write("Enter the movie title: ");
-                    newMovie.Title = Console.ReadLine();
-
-                    // Check for duplicate movie title
-                    foreach (Movie movie in movies)
-                    {
-                        lowerCaseMovieTitles.Add(movie.Title.ToLower());
-                    }
-
-                    if (lowerCaseMovieTitles.Contains(newMovie.Title.ToLower()))
-                    {
-                        Console.WriteLine("That movie has already been entered.");
-                    }
-                    else
-                    {
-                        newMovie.Id = movies[^1].Id + 1;
-
-                        newMovie.Genres = AddGenres();
-
-                        // Check for comma in title
-                        newMovie.Title = newMovie.Title.IndexOf(',') != -1 ? $"\"{newMovie.Title}\"" : newMovie.Title;
-
-                        // Create newMovie in file and write to console
-                        Console.WriteLine(newMovie);
-                        StreamWriter sw = new StreamWriter(file, true);
-                        sw.WriteLine(newMovie);
-                        sw.Close();
-                    }
-
-                    Console.Write("\nAdd another movie (Y/N)? ");
-                } while (Console.ReadLine().ToUpper() == "Y");
+                AddNewMovie(file);
             }
             else if (!File.Exists(file))
             {
                 Console.WriteLine($"{file} does not exist.");
             }
         } while (menuOption == "1" || menuOption == "2");
+    }
+
+    public static List<string> AddGenres()
+    {
+        List<string> genres = new();
+        // Loop to add genres
+        while (true)
+        {
+            Console.Write("Enter a genre (or N to exit): ");
+            string genre = Console.ReadLine();
+            if (genre.ToUpper() != "N" && genre != null)
+            {
+                genres.Add($"{genre}");
+            }
+            else
+            {
+                if (genres.Count == 0)
+                {
+                    genres.Add($"(no genres listed)");
+                }
+                return genres;
+            }
+        }
+    }
+
+    public static void AddNewMovie(string file)
+    {
+        // Loop to add movies
+        do
+        {
+            List<Movie> movies = GetMoviesList(file);
+            List<string> lowerCaseMovieTitles = new();
+            Movie newMovie = new();
+            Console.Write("Enter the movie title: ");
+            newMovie.Title = Console.ReadLine();
+
+            // Check for duplicate movie title
+            foreach (Movie movie in movies)
+            {
+                lowerCaseMovieTitles.Add(movie.Title.ToLower());
+            }
+
+            if (lowerCaseMovieTitles.Contains(newMovie.Title.ToLower()))
+            {
+                Console.WriteLine("That movie has already been entered.");
+            }
+            else
+            {
+                newMovie.Id = movies[^1].Id + 1;
+
+                newMovie.Genres = AddGenres();
+
+                // Check for comma in title
+                newMovie.Title = newMovie.Title.IndexOf(',') != -1 ? $"\"{newMovie.Title}\"" : newMovie.Title;
+
+                // Create newMovie in file and write to console
+                Console.WriteLine(newMovie);
+                StreamWriter sw = new StreamWriter(file, true);
+                sw.WriteLine(newMovie);
+                sw.Close();
+            }
+            Console.Write("\nAdd another movie (Y/N)? ");
+        } while (Console.ReadLine().ToUpper() == "Y");
+    }
+
+    public static void DisplayAllMovies(string file)
+    {
+        // nuget Trival.Console (7.0.0) package for paging
+        // It does have some bugs with press the down arrow keys too fast
+        List<Movie> movies = GetMoviesList(file);
+        var col = new Trivial.Collection.SelectionData<string>();
+        for (int i = 0; i < movies.Count; i++)
+        {
+            // Add movie to display
+            col.Add(movies[i].DisplayMovie());
+        }
+
+        // Create options for display
+        var options = new SelectionConsoleOptions
+        {
+            Tips = "Tips: You can use arrow key to select and press ENTER key to exit.",
+            TipsForegroundConsoleColor = ConsoleColor.Yellow,
+            SelectedPrefix = "> ",
+            Prefix = " ",
+            Column = 2,
+            MaxRow = 20
+        };
+
+        // Pause to let user read and and see full description of selected item
+        DefaultConsole.Select(col, options);
+        Console.WriteLine(); // Spacer line for returning to main menu
     }
 
     public static List<Movie> GetMoviesList(string file)
@@ -144,26 +169,4 @@ internal class Program
         return movies;
     }
 
-    public static List<string> AddGenres()
-    {
-        List<string> genres = new();
-        // Loop to add genres
-        while (true)
-        {
-            Console.Write("Enter a genre (or N to exit): ");
-            string genre = Console.ReadLine();
-            if (genre.ToUpper() != "N" && genre != null)
-            {
-                genres.Add($"{genre}");
-            }
-            else
-            {
-                if (genres.Count == 0)
-                {
-                    genres.Add($"(no genres listed)");
-                }
-                return genres;
-            }
-        }
-    }
 }
